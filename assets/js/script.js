b18fb077ee4b416dbf172d4c9f6a674b095e9c7c
Apon wuz here <frost_bird@hotmail.com>
2022-06-29 10:17:38 -0500

    Created files

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        choiceA : "Huge Tower Meets London",
+        choiceB : "Hyper Text Markup Language",
+        choiceC : "Hyper Lynx Makes Lunch",
+        choiceD : "Home Tool for Marking Luggage",
+        correct : "B"
+    
+    },{
+        question : "What does HTML stand for?",
+        choiceA : "Huge Tower Meets London",
+        choiceB : "Hyper Text Markup Language",
+        choiceC : "Hyper Lynx Makes Lunch",
+        choiceD : "Home Tool for Marking Luggage",
+        correct : "B"
+    
+    },{
+        question : "What does HTML stand for?",
+        choiceA : "Huge Tower Meets London",
+        choiceB : "Hyper Text Markup Language",
+        choiceC : "Hyper Lynx Makes Lunch",
+        choiceD : "Home Tool for Marking Luggage",
+        correct : "B"
+    
+    },{
+        question : "What does HTML stand for?",
+        choiceA : "Huge Tower Meets London",
+        choiceB : "Hyper Text Markup Language",
+        choiceC : "Hyper Lynx Makes Lunch",
+        choiceD : "Home Tool for Marking Luggage",
+        correct : "B"
+    
+    },{
+        question : "What does HTML stand for?",
+        choiceA : "Huge Tower Meets London",
+        choiceB : "Hyper Text Markup Language",
+        choiceC : "Hyper Lynx Makes Lunch",
+        choiceD : "Home Tool for Marking Luggage",
+        correct : "B"
+    
+    },];
