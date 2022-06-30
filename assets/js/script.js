7509f08602f4b31cccb15efc78fc9b62e9b50ec6
Apon wuz here <frost_bird@hotmail.com>
2022-06-30 12:37:25 -0500

    created baseline html&css, as well as, added a reset css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+var testBox = getElementById(testbox);
+var scores = getElementById(scoresTab);
+var startButton = getElementById(start);
+var timer = getElementById(timer);
+
 let questions = [
     {
         question : "What does HTML stand for?",
@@ -8,35 +13,37 @@ let questions = [
         correct : "B"
     
     },{
-        question : "What does HTML stand for?",
-        choiceA : "Huge Tower Meets London",
-        choiceB : "Hyper Text Markup Language",
-        choiceC : "Hyper Lynx Makes Lunch",
-        choiceD : "Home Tool for Marking Luggage",
-        correct : "B"
+        question : "Which of these terms can be used to identify a variable?",
+        choiceA : "'const'",
+        choiceB : "'var'",
+        choiceC : "'let'",
+        choiceD : "All of the Above",
+        correct : "D"
     
     },{
-        question : "What does HTML stand for?",
-        choiceA : "Huge Tower Meets London",
+        question : "What is the method used to draft out your code before implementing it?",
+        choiceA : "psuedo code",
         choiceB : "Hyper Text Markup Language",
-        choiceC : "Hyper Lynx Makes Lunch",
-        choiceD : "Home Tool for Marking Luggage",
-        correct : "B"
+        choiceC : "google drive",
+        choiceD : "Panic and Pray",
+        correct : "A"
     
     },{
-        question : "What does HTML stand for?",
-        choiceA : "Huge Tower Meets London",
-        choiceB : "Hyper Text Markup Language",
-        choiceC : "Hyper Lynx Makes Lunch",
-        choiceD : "Home Tool for Marking Luggage",
-        correct : "B"
+        question : "Pick a number.",
+        choiceA : "'15'",
+        choiceB : "fifty-six",
+        choiceC : "12",
+        choiceD : "Stephen King",
+        correct : "C"
     
     },{
-        question : "What does HTML stand for?",
-        choiceA : "Huge Tower Meets London",
-        choiceB : "Hyper Text Markup Language",
-        choiceC : "Hyper Lynx Makes Lunch",
-        choiceD : "Home Tool for Marking Luggage",
-        correct : "B"
+        question : "No really, pick a number..",
+        choiceA : "No",
+        choiceB : "You can't make me",
+        choiceC : "1",
+        choiceD : "Ur a number",
+        correct : "C"
     
-    },];
+    },
+];
+
